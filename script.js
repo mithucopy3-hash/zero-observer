2317aac46affdab2d435f0389d46f53fa6968560
mithucopy3-hash <mithucopy3@gmail.com>
2026-07-06 22:14:55 -0700

    aa

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Zero Observer Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const target = document.querySelector(this.getAttribute("href"));
+
+            if(target){
+                target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+
+});
